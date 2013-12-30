b31d0f31fbda0afc3bc152bb35308acdfa3e49db
Ophir LOJKINE <pere.jobs@gmail.com>
2013-12-30 22:53:49 +0100

    Delete useless files

--- dezoomify/proxy.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-$url = get_magic_quotes_gpc() ? stripslashes($_GET['url']) : $_GET["url"];
-if (strpos($url, "http") !== 0) die("Only http requests are allowed.");
-readfile($url);
-?>
